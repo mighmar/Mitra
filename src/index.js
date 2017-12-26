ba93cd0825e8e9dcb1a1bc8ef29325cfd574ae31
Stefan Maksimovic <mi12078@alas.math.rs>
2017-12-26 09:46:15 +0100

    Omoguceno registrovanje/logovanje

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RegisterLogin from './RegisterLogin';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<RegisterLogin />, document.getElementById('root'));
 registerServiceWorker();
