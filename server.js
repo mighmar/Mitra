4016f68f5bb28010c7d433bc241e4c89b192aff0
Filip Novovic <novke93@gmail.com>
2018-01-17 23:59:13 +0100

    Povezivanje servera i klijenta

--- server.js
+++ server.js
@@ -4,13 +4,25 @@ var express = require("express"),
 
 var routes  = require("./routes"),
     config  = require("./config").server;
-    mongodb = require("./mongodb");
 
 
-mongodb.connect(err => {
+app.set("port",process.env.PORT || 3001)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+    //mongodb = require("./mongodb");
+
+
+/*mongodb.connect(err => {
    db = mongodb.getDB();
    var sheets = db.collection('sheets');
    routes.route(app);
    app.listen(config.port, () => {
    })
-});
+});*/
+
+app.listen(app.get("port"), () => {
+    console.log(`Find the server at: http://localhost:${app.get("port")}/`); // eslint-disable-line no-console
+  });
