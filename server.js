e13585f29c03e4f3947ad783c090f1884ff95217
Marko Crnobrnja <fdsafk@jiaofga.com>
2018-01-28 05:51:16 +0100

    Revert "sanity check"
    This reverts commit caef0356e89186a29dadc477ff9f017d639b2a25.

--- server.js
+++ server.js
@@ -12,32 +12,18 @@ var config  = require("./config").server;
 app.use(cors());
 app.set("port", config.port)
 
+var intervalID = setInterval(function(){console.log("I'm alive!");}, 50000);
+
 if (process.env.NODE_ENV === "production") 
    app.use(express.static("client/build"));
   
 var server = app.listen(app.get("port")); 
 
 console.log("connecting to mongo");
-/*mongodb.connect().then(db => {
+mongodb.connect().then(db => {
    console.log("connecting sockets");
    sockets.connect(server, db, mongodb.OID);
 
 }).catch(err => {
    console.error(err);
 }); 
-*/
-
-var socketIo  = require("socket.io");
-var io = socketIo(server);
-io.on('connection', function (socket) {
-   peraRow = 1;
-   socket.on('open sheet', function (data) { 
-      console.log("User ", data.name, " Opening sheet ", data.sheetId);
-      socket.emit('sheet data', {sheet: {}, users: [{username:"Pera", color: "red", row: 2, col: 2}]}); 
-   }
-   socket.on('select cell', function (data) {
-      console.log("Selecting cell ", data.row, ":", data.col);
-      socket.emit('cell selected', [{username:"Pera", color: "red", row: peraRow++, col: 2}]);
-   }
-}
-
