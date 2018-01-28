f9a7eb5337b75a3b2afa88e7826841dba57929dc
Marko Crnobrnja <fdsafk@jiaofga.com>
2018-01-28 02:22:24 +0100

    vraćamo port

--- mongodb.js
+++ mongodb.js
@@ -4,12 +4,9 @@ var util = require('util');
 var config = require('./config').mongodb;
 var _db;
 
-//var uri = util.format('mongodb://%s:%s@%s:%d/%s',
-var uri = util.format('mongodb://%s:%s@%s/%s',
+var uri = util.format('mongodb://%s:%s@%s:%d/%s',
     config.username, config.password, config.host,
-     config.databaseName);
-    //config.username, config.password, config.host,
-    //config.port, config.databaseName);
+    config.port, config.databaseName);
 var options = {};
 
 function connectDB () {
