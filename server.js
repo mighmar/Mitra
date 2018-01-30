54ffe4daa4f50dbdeae03df2b46dc1725ad87d24
Marko Crnobrnja <fdsafk@jiaofga.com>
2018-01-30 23:15:45 +0100

    sada?

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ var express = require("express"),
     app     = express(),
     cors    = require("cors");
 
+
 var config  = require("./config").server;
     mongodb = require("./mongodb");
     sockets = require("./sockets");
+ 
 
 
-app.use(cors(credentials:true, origin:true));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-})
 app.set("port", config.port)
-//app.use(cors());
+app.use(cors());
+
+//var intervalID = setInterval(function(){console.log("I'm alive!");}, 50000);
   
 var server = app.listen(app.get("port")); 
 
